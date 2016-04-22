82972f860d5475b216523a8b7b73162c7e2aa7c1
thegregalan <socciarelli5@gmail.com>
2016-04-22 09:27:20 -0400

    BARCODE PROJECT: BINARY CODE

---
+++ new binary barcode.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main (){
+
+int b;
+cout << "Enter the number of bars in your barcode (between 1 and 21): ";
+cin >> b;
+
+char binary[b];
+int barcode[b];
+
+if (b >= 1 && b <= 21){
+
+    int c;
+    for (c=0; c<b; c++){
+        cout << "Enter a w for a wide bar or an n for a narrow bar: ";
+        cin >> binary[c];
+    }
+
+    int d;
+    for (d=0; d<b; d++){
+        if (binary[d] == 'w')
+            barcode[d] = 1;
+        else if (binary[d] == 'n')
+            barcode[d] = 0;
+            cout << barcode[d];
+    }
+
+}
+
+return 0;
+
+}
