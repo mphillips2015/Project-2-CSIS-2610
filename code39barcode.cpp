e14c5335efdeb8f1e0bc025fd0ebe52eeb68841c
thegregalan <socciarelli5@gmail.com>
2016-04-23 20:47:49 -0400

    Added files via upload

---
+++ code39barcode.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main (){
+int b;
+
+if (b=9){
+    char binary[b];
+    int barcode[b];
+    int c;
+    for (c=0; c<b; c++){
+        cout << "Enter a w for a wide bar or an n for a narrow bar: ";
+        cin >> binary[c];
+    }
+
+    int d;
+    for (d=0; d<b; d++){
+        if (binary[d] == 'w')
+            barcode[d] = 1;
+        else if (binary[d] == 'n')
+            barcode[d] = 0;
+            cout << barcode[d];
+    }
+}
+return 0;
+}
